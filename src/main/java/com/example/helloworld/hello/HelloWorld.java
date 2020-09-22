825d51ba59507061a46f5a58112d79733c484481
Atef Sendesni <atefsendesni512@hotmail.com>
2020-09-22 21:00:15 +0200

    Added getMapping and restController

--- src/main/java/com/example/helloworld/hello/HelloWorld.java
+++ src/main/java/com/example/helloworld/hello/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.example.helloworld.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorld {
 
+    @GetMapping
     public String hello(){
         return "Hello world";
     }
